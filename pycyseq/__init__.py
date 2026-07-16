cfbdf2bbd4f804a31b02ea9efc3d8d8e8cbe3f50
marcpaga <marc.pages@live.com>
2026-07-16 12:46:20 +0200

    add read function in init for easier import

--- pycyseq/__init__.py
+++ pycyseq/__init__.py
@@ -4,3 +4,4 @@ from .single_strand import SingleStrandAlignment as SingleStrandAlignment
 from .double_strand import DoubleStrandAlignment as DoubleStrandAlignment
 from .alignment_group import AlignmentGroup as AlignmentGroup
 from .alignment import Alignment as Alignment
+from .io import read_cyseq_alignment_file as read_cyseq_alignment_file
